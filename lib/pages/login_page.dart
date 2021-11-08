9aea551dbf03688e97bf9ae79a9603f380fc2adf
UmeshRai508 <Umesh.Rai508@outlook.com>
2021-11-08 02:24:01 +0000

    added login form

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
+// Widgets
+import '../widgets/custom_input_fields.dart';
+
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return _LoginPageState();
@@ -8,12 +13,68 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+  final _loginFormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * .97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [_pageTitle(), _loginForm()],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return Container(
+      height: _deviceHeight * 0.1,
+      child: const Text(
+        "Force Tracker",
+        style: TextStyle(
+            color: Colors.white, fontWeight: FontWeight.w600, fontSize: 28),
+      ),
+    );
+  }
+
+  Widget _loginForm() {
+    return Container(
+      height: _deviceHeight * 0.18,
+      child: Form(
+        key: _loginFormKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+                onSaved: (_vlaue) {},
+                regEx:
+                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                hintText: "Email",
+                obscureText: false),
+            CustomTextFormField(
+                onSaved: (_vlaue) {},
+                regEx: r".{8,}",
+                hintText: "Password",
+                obscureText: true),
+          ],
+        ),
+      ),
+    );
   }
 }
