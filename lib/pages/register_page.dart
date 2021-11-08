15f9a486fca8d3563eb35e338eb7f1dee4119829
Umesh Rai <UmeshRai508@outlook.com>
2021-11-08 16:58:45 +0000

    testing image load

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,39 +1,119 @@
-// Packages
+//Packages
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
-// Services
+//Services
 import '../services/media_service.dart';
 import '../services/database_service.dart';
 import '../services/cloud_storage_service.dart';
+import '../services/navigation_service.dart';
 
-// Widgets
+//Widgets
 import '../widgets/custom_input_fields.dart';
 import '../widgets/rounded_button.dart';
-
-// Providers
+import '../widgets/rounded_image.dart';
+//Providers
 import '../providers/authentication_provider.dart';
 
 class RegisterPage extends StatefulWidget {
-  const RegisterPage({Key? key}) : super(key: key);
-
   @override
-  _RegisterPageState createState() => _RegisterPageState();
+  State<StatefulWidget> createState() {
+    return _RegisterPageState();
+  }
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+  late DatabaseService _db;
+  late CloudStorageService _cloudStorage;
+  late NavigationService _navigation;
+
+  String? _email;
+  String? _password;
+  String? _name;
+  PlatformFile? _profileImage;
+
+  final _registerFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    _auth = Provider.of<AuthenticationProvider>(context);
+    _db = GetIt.instance.get<DatabaseService>();
+    _cloudStorage = GetIt.instance.get<CloudStorageService>();
+    _navigation = GetIt.instance.get<NavigationService>();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Register Page"),
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: _deviceWidth * 0.03,
+          vertical: _deviceHeight * 0.02,
+        ),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profileImageField(),
+            SizedBox(
+              height: _deviceHeight * 0.05,
+            ),
+            // _registerForm(),
+            SizedBox(
+              height: _deviceHeight * 0.05,
+            ),
+            // _registerButton(),
+            SizedBox(
+              height: _deviceHeight * 0.02,
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _profileImageField() {
+    return GestureDetector(
+      onTap: () {
+        GetIt.instance.get<MediaService>().pickImageFromLibrary().then(
+          (_file) {
+            setState(
+              () {
+                print(_file);
+                _profileImage = _file;
+                print(":::>> New Image.");
+              },
+            );
+          },
+        );
+      },
+      child: () {
+        if (_profileImage != null) {
+          return RoundedImageFile(
+            key: UniqueKey(),
+            image: _profileImage!,
+            size: _deviceHeight * 0.15,
+          );
+        } else {
+          return RoundedImageNetwork(
+            key: UniqueKey(),
+            imagePath: "https://i.pravatar.cc/150?img=65",
+            size: _deviceHeight * 0.15,
+          );
+        }
+      }(),
     );
   }
 }
